9f9f920e83b8466bd1f1327e5cadbc89b8d22423
Yakov Levin <y.levin95@gmail.com>
2023-11-22 17:54:16 +0300

    Add start template

---
+++ __tests__/half.test.js
@@ -0,0 +1,8 @@
+// @ts-check
+
+import { test, expect } from '@jest/globals';
+import half from '../index.js';
+
+test('half', () => {
+  expect(half(6)).toBe(3);
+});
---
+++ src/half.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+// import _ from 'lodash';
+
+/**
+ * @param {Number} num
+ */
+export default (num) => {
+  const result = num / 2;
+  return result;
+};
