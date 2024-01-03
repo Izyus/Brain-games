7b498e9484af4b994de6fa43902ad52c00d737bc
Yakov L <you@example.com>
2024-01-03 22:56:04 +0300

    isEven game add

---
+++ bin/brain-even.js
@@ -0,0 +1,48 @@
+import readlineSync from "readline-sync";
+console.log("Welcome to the Brain Games!");
+const names = readlineSync.question("May I have your name? ");
+console.log(`Hello, ${names}`);
+console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
+
+const playRound = () => {
+  const getNumb = (min = 1, max = 15) =>
+    Math.floor(min + Math.random() * (max + 1 - min));
+  const newNumb = getNumb();
+  const numbCheck = (newNumb) => (newNumb % 2 == 0 ? "yes" : "no");
+  console.log(numbCheck(newNumb));
+  const question = readlineSync.question(`Question: ${newNumb} `);
+
+  if (numbCheck(newNumb) === question.toLowerCase()) {
+    console.log("Correct!");
+    return true;
+  } else {
+    console.log(
+      `'${question}' is wrong answer ;(. Correct answer was '${numbCheck(
+        newNumb
+      )}'.`
+    );
+    console.log(`Let's try again, ${names}!`);
+  }
+};
+const playGame = () => {
+  for (let i = 0; i < 3; i++) {
+    const isCorrect = playRound();
+    if (!isCorrect) {
+      return;
+    }
+  }
+  console.log(`Congratulations, ${names}!`);
+};
+playGame();
+// let number = randomInteger(1, 15);
+// const firstQuestion = readlineSync.question(`Question: ${number} `);
+// const checkNumber = (number) => {
+//   if (number % 2 == 0) {
+//     return "yes";
+//   } else {
+//     return "no";
+//   }
+// };
+// if (firstQuestion.toLowerCase() == checkNumber(number)) {
+//   console.log("Correct!");
+// }
