03eca6b8e9af1ecb22ec50a4678d8e48b26a2eeb
Yakov L <you@example.com>
2024-01-08 21:15:53 +0300

    go

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -2,28 +2,28 @@
 /* eslint-disable no-console */
 import readlineSync from "readline-sync";
 import { playGame, names, getNumb } from "../../src/index.js";
-
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
 const playRound = () => {
   const number = getNumb(1, 100);
   const question = readlineSync.question(`Question: ${number} `);
   function isPrime(n) {
-    for (let i = 2; i < n; i) {
+    for (let i = 2; i < n; i++) {
       if (n % i === 0) return "no";
     }
     if (n > 1) {
       return "yes";
     }
   }
-
-  if (question === isPrime(number)) {
+  if (question == isPrime(number)) {
     console.log("Correct!");
     return true;
+  } else {
+    console.log(
+      `'${question}' is wrong answer ;(. Correct answer was '${isPrime(
+        number
+      )}'.`
+    );
+    console.log(`Let's try again, ${names}!`);
   }
-  console.log(
-    `'${question}' is wrong answer ;(. Correct answer was '${isPrime(number)}'.`
-  );
-  console.log(`Let's try again, ${names}!`);
 };
-
 playGame(playRound, names);
