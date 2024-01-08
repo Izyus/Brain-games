313552a96d8c8a70a852c089ed888e4da0fb5524
Yakov L <you@example.com>
2024-01-08 21:07:02 +0300

    gcd

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -1,34 +1,30 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
-import readlineSync from "readline-sync";
 import { playGame, names, getNumb } from "../../src/index.js";
-
-console.log("Find the greatest common divisor of given numbers.");
+import readlineSync from "readline-sync";
+console.log(`Find the greatest common divisor of given numbers.`);
 const playRound = () => {
   const firstNumber = getNumb();
   const secondNumber = getNumb();
-
   const numbCheck = (firstNumber, secondNumber) => {
     if (secondNumber > firstNumber) return numbCheck(secondNumber, firstNumber);
     if (!secondNumber) return firstNumber;
     return numbCheck(secondNumber, firstNumber % secondNumber);
   };
-
   const question = readlineSync.question(
     `Question: ${firstNumber} ${secondNumber} `
   );
-
-  if (numbCheck(firstNumber, secondNumber) === question.toLowerCase()) {
+  if (numbCheck(firstNumber, secondNumber) == question.toLowerCase()) {
     console.log("Correct!");
     return true;
+  } else {
+    console.log(
+      `'${question}' is wrong answer ;(. Correct answer was '${numbCheck(
+        firstNumber,
+        secondNumber
+      )}'.`
+    );
+    console.log(`Let's try again, ${names}!`);
   }
-  console.log(
-    `'${question}' is wrong answer ;(. Correct answer was '${numbCheck(
-      firstNumber,
-      secondNumber
-    )}'.`
-  );
-  console.log(`Let's try again, ${names}!`);
 };
-
 playGame(playRound, names);
