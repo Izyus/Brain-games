ef9aaa1ced9f23b55252b35716dbb2799c261380
Yakov L <you@example.com>
2024-01-08 18:27:53 +0300

    add new game

---
+++ bin/games/brain-progression.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+import readlineSync from "readline-sync";
+import { playGame, names, getNumb } from "../../src/index.js";
+console.log(`What number is missing in the progression?`);
+const playRound = () => {
+  const increaseCount = getNumb(1, 10);
+  const arr = [];
+  for (let i = 0; i <= 9; i++) {
+    arr[i] = i * increaseCount + increaseCount;
+  }
+  const randomIndex = getNumb(1, 9);
+  const temp = arr[randomIndex];
+  arr[randomIndex] = "..";
+  const question = readlineSync.question(`Question: ${arr.join(" ")} `);
+  if (question == temp) {
+    console.log("Correct!");
+    return true;
+  } else {
+    console.log(
+      `'${question}' is wrong answer ;(. Correct answer was '${temp}'.`
+    );
+    console.log(`Let's try again, ${names}!`);
+  }
+};
+
+playGame(playRound, names);
