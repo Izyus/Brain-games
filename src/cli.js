6e345fc1eac5a41c88def7662adfae6626002f00
Yakov L <you@example.com>
2024-01-08 23:24:44 +0300

    fix eslint

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,9 @@ import readlineSync from 'readline-sync';
 const askNameAndGreet = () => {
   const name = readlineSync.question('May I have your name? ');
 
+  // eslint-disable-next-line no-console
   console.log(`Hello, ${name}!`);
 };
 
+// eslint-disable-next-line import/prefer-default-export
 export { askNameAndGreet };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
 
 export const names = readlineSync.question('May I have your name? ');
+// eslint-disable-next-line no-console
 console.log(`Hello, ${names}`);
 
+// eslint-disable-next-line no-shadow
 export const playGame = (playRound, names) => {
   for (let i = 0; i < 3; i += 1) {
     const isCorrect = playRound();
@@ -10,8 +12,8 @@ export const playGame = (playRound, names) => {
       return;
     }
   }
+  // eslint-disable-next-line no-console
   console.log(`Congratulations, ${names}!`);
 };
 
-export const getNumb = (min = 1, max = 50) =>
-Math.floor(min + Math.random() * (max + 1 - min));
+export const getNumb = (min = 1, max = 50) => Math.floor(min + Math.random() * (max + 1 - min));
