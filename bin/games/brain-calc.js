bf252542219798b66cebf0ac5e33f90154661958
Yakov L <you@example.com>
2024-01-08 21:00:26 +0300

    erf

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -1,46 +1,36 @@
 #!/usr/bin/env node
-/* eslint-disable no-shadow */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable eqeqeq */
 /* eslint-disable no-console */
 import readlineSync from "readline-sync";
 import { playGame, names, getNumb } from "../../src/index.js";
-
-console.log("What is the result of the expression?");
-
-// eslint-disable-next-line consistent-return
+console.log(`What is the result of the expression?`);
 export const playRound = () => {
   const newNumb1 = getNumb();
   const newNumb2 = getNumb();
   const operation = ["+", "-", "*"];
   const randomOperator = operation[getNumb(0, 2)];
-  function numbCheck(newNumb1, newNumb2, randomOperator) {
+  const numbCheck = (newNumb1, newNumb2, randomOperator) => {
     if (randomOperator == "+") {
       return newNumb1 + newNumb2;
-    }
-    if (randomOperator == "-") {
+    } else if (randomOperator == "-") {
       return newNumb1 - newNumb2;
+    } else {
+      return newNumb1 * newNumb2;
     }
-    return newNumb1 * newNumb2;
-  }
-
+  };
   const question = readlineSync.question(
     `Question: ${newNumb1} ${randomOperator} ${newNumb2} `
   );
-
-  if (
-    numbCheck(newNumb1, newNumb2, randomOperator) === question.toLowerCase()
-  ) {
+  if (numbCheck(newNumb1, newNumb2, randomOperator) == question.toLowerCase()) {
     console.log("Correct!");
     return true;
+  } else {
+    console.log(
+      `'${question}' is wrong answer ;(. Correct answer was '${numbCheck(
+        newNumb1,
+        newNumb2
+      )}'.`
+    );
+    console.log(`Let's try again, ${names}!`);
   }
-  console.log(
-    `'${question}' is wrong answer ;(. Correct answer was '${numbCheck(
-      newNumb1,
-      newNumb2
-    )}'.`
-  );
-  console.log(`Let's try again, ${names}!`);
 };
-
 playGame(playRound, names);
